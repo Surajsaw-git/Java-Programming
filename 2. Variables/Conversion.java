f6930f561432540523135e5fdd2110d60a296e71
suraj saw <surajsaw7050500@gmail.com>
2026-02-06 23:45:46 +0530

    update code for Conversion of data type

--- 2. Variables/Conversion.java
+++ 2. Variables/Conversion.java
@@ -8,11 +8,17 @@ class Conversion
 {
     public static void main(String a[])
     {
-        int num;
-        num=50.62;
-        double num1;
-        num1=50;
-        System.out.println(num);
-        System.out.println(num1);
+        int num=10;
+        double num1=num;   // int to double 
+
+        System.out.println("Int value : "+ num);
+        System.out.println("Double value : "+ num1);
+
+
+        double x=15.85;
+        int y = (int) x;
+
+        System.out.println("Double value : "+ x);
+        System.out.println("Int value : "+ y);
     }
 }
