97448bce6a83738fd5bc420bec7211e6bebfcf1e
suraj saw <surajsaw7050500@gmail.com>
2026-02-07 00:50:33 +0530

    use explicit casting to convert byte into int.

--- 4. Type Conversion/byte_int.java
+++ 4. Type Conversion/byte_int.java
@@ -32,10 +32,24 @@ class byte_int
         // PS D:\Programming\Java\4. Type Conversion>
 
 
-        byte c;
-        int d=56;
-        c=d;  // this is not posible, we can convert int into byte data type.
+        // byte c;
+        // int d=56;
+        // c=d;  // this is not posible, we can convert int into byte data type.
 
+        // System.out.println(c);
+        // System.out.println(d);
+
+
+
+
+        byte c;
+        int d=25;
+        c=(byte)d;  // this is posible, we can convert int into byte data type.
+
+        // here we do casting
+
+        //To convert an int to a byte in Java, you must use explicit casting (also known as narrowing conversion). 
+        //This is required because an int (32 bits) is larger than a byte (8 bits), which means data loss is possible.
         System.out.println(c);
         System.out.println(d);
 
