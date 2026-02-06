3cbd2fab5e243e54d6eac9d8f5b588c7ba52da53
suraj saw <surajsaw7050500@gmail.com>
2026-02-07 00:42:46 +0530

    Conversion between byte into int and int into byte

---
+++ 4. Type Conversion/byte_int.java
@@ -0,0 +1,43 @@
+//Conversion between byte into int and int into byte
+
+class byte_int
+{
+    public static void main(String[] args)
+    {
+        byte b=127;
+        int a;
+        a=b;  // this is posible, we can convert byte into int data type.
+
+        System.out.println(a);
+        System.out.println(b);
+
+        // PS D:\Programming\Java\3. Data Types> cd "d:\Programming\Java\4. Type Conversion\" ; if ($?) { javac byte_int.java } ; if ($?) { java byte_int }      
+        // 127
+        // 127
+        // PS D:\Programming\Java\4. Type Conversion>
+
+
+        // byte c;
+        // int d=256;
+        // c=d;  // this is not posible, we can convert int into byte data type.
+
+        // System.out.println(c);
+        // System.out.println(d);
+
+        // PS D:\Programming\Java\4. Type Conversion> cd "d:\Programming\Java\4. Type Conversion\" ; if ($?) { javac byte_int.java } ; if ($?) { java byte_int } 
+        // byte_int.java:22: error: incompatible types: possible lossy conversion from int to byte
+        //         c=d;  // this is posible, we can convert byte into int data type.
+        //           ^
+        // 1 error
+        // PS D:\Programming\Java\4. Type Conversion>
+
+
+        byte c;
+        int d=56;
+        c=d;  // this is not posible, we can convert int into byte data type.
+
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
