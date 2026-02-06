6b7a9ce516d5db7247025de7193de47d1ed9838d
suraj saw <surajsaw7050500@gmail.com>
2026-02-07 00:32:10 +0530

    character can increment like integer.

---
+++ 3. Data Types/char_value.java
@@ -0,0 +1,15 @@
+// playing with character value 
+
+class char_value
+{
+    public static void main(String[] args)
+    {
+        char c='a';
+        System.out.println(c);
+
+        // character can increment like integer. 
+        c++;
+        System.out.println(c);
+
+    }
+}
