ccd15cf4fe495920b079a1c0142c82ff75279767
suraj saw <surajsaw7050500@gmail.com>
2026-02-07 00:05:02 +0530

    Variable declearation for float

--- 2. Variables/Variables_declare.java
+++ 2. Variables/Variables_declare.java
@@ -5,20 +5,33 @@ class Variables_declare
     {
         int num;
         double avg_marks;
-        float marks;
+        float marks;  // in java float will not work so we have to specifie that it is a float value.
+        float mark_math;
         char symball;
         String name;
 
         num=45;
         avg_marks=95.65;
-        marks=50;
+        // mark_math=85.95;   // this will show an error while exicutation the program. I have attached the error massage.
+        marks=50.65f;   // this is correct 
         symball='%';
         name="Suraj Saw";
 
         System.out.println(name);
         System.out.println(num);
         System.out.println(marks);
+        //System.out.println(mark_math);
         System.out.print(avg_marks);
         System.out.println(symball);
     }
 }
+
+
+
+
+// PS D:\Programming\Java> cd "d:\Programming\Java\2. Variables\" ; if ($?) { javac Variables_declare.java } ; if ($?) { java Variables_declare }
+// Variables_declare.java:15: error: incompatible types: possible lossy conversion from double to float
+//         mark_math=85.95;   // this will show an error while exicutation the program
+//                   ^
+// 1 error
+// PS D:\Programming\Java\2. Variables> 
