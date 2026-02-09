663edcac5c9047f8761fe0cb9a3394f82e8eccc3
suraj saw <surajsaw7050500@gmail.com>
2026-02-09 13:59:57 +0530

    Check character Type code in java

---
+++ 7. User Input/CheckCharacterType.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CheckCharacterType {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z') {
+            System.out.println("It is an Alphabet");
+        }
+        else if (ch >= '0' && ch <= '9') {
+            System.out.println("It is a Digit");
+        }
+        else {
+            System.out.println("It is a Special Character");
+        }
+    }
+}
